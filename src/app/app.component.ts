682998ab30e0ea35d89484a45c5c8e9b2b827af4
erickubuntu <gestionecesaria@gmail.com>
2023-12-18 13:41:14 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'my-app-angular';
+  title = 'Fulanito de tal';
+  subtitle = 'es la persona que esta logeada';
 }
